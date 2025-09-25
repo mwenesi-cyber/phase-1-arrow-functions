50dd2902484220964b690f03e3e55c1a0fb81af9
mwenesi-cyber <mwenesibrandon1@gmail.com>
2025-09-25 15:34:33 +0300

    Completed arrow functions lab

---
+++ index.js
@@ -0,0 +1,13 @@
+// 1. Function expression (review)
+const divide = function(a, b) {
+  return a / b;
+};
+
+// 2. Arrow function - one-liner
+const square = x => x * x;
+
+// 3. Arrow function - two parameters
+const add = (a, b) => a + b;
+
+// 4. Arrow function - no parameters
+const greet = () => "Hello, world!";
